15ae37aeae39b6472323fc51f289ff0e98b5e41b
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-19 13:55:09 -0300

    feat: create server

---
+++ src/service.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'express-async-errors';
+import 'dotenv/config';
+import routes from './routes';
+
+const { SERVER_PORT } = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', routes);
+
+app.use('middleware error');
+
+app.listen(SERVER_PORT, () => console.log('Listening at', SERVER_PORT));
