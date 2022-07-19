0c27070c5ab2e1e360d75324f65fc86071aac8d5
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-19 17:30:47 -0300

    feat: create orders migration

---
+++ src/database/migrations/20220719192445-create-order.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      clientId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'client_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Clients',
+          key: 'id',
+        },
+      },
+      assetId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'asset_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Assets',
+          key: 'id',
+        },
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+      },
+      value: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      created: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Orders');
+  },
+};
