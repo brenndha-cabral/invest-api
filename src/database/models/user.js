9ec06776f66140880c5d481fbcf5735f3c698c9d
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-19 17:31:04 -0300

    feat: create client model

---
+++ src/database/models/user.js
@@ -0,0 +1,26 @@
+export default (sequelize, DataTypes) => {
+  const Client = sequelize.define('Client', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    cfp: DataTypes.STRING,
+    image: DataTypes.STRING,
+    adm: DataTypes.BOOLEAN,
+    balance: DataTypes.DECIMAL(10, 2),
+    phone: DataTypes.STRING,
+    address: DataTypes.STRING,
+    created: DataTypes.DATE,
+  }, {
+    timestamps: false,
+    tableName: 'Clients',
+  });
+
+  Client.associate = (models) => {
+    Client.hasMany(models.Order, {
+      foreignKey: 'clientId',
+      as: 'orders',
+    });
+  };
+
+  return Client;
+};
