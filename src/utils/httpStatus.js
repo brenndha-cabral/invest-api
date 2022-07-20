ba844072a779dd06671c90986747b18053d4f646
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-20 15:48:33 -0300

    feat: create statusCode and statusResponse

---
+++ src/utils/httpStatus.js
@@ -0,0 +1,25 @@
+const statusCode = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  UNPROCESSABLE_ENTITY: 422,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+const statusResponse = {
+  BAD_REQUEST: 'Bad Request',
+  NOT_FOUND: 'Not found',
+  CONFLICT: 'Conflict',
+  CREATED: 'Created',
+  UNAUTHORIZED: 'Unauthorized',
+  UNPROCESSABLE_ENTITY: 'Unprocessable Entity',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+};
+
+module.exports = {
+  statusCode,
+  statusResponse,
+};
