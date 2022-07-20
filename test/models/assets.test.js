da9cb6ce1e5d8a945c2504eeaf534c67b086daa2
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-19 23:07:40 -0300

    test: create assets model test

---
+++ test/models/assets.test.js
@@ -0,0 +1,46 @@
+import chai, { expect } from 'chai';
+
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai)
+
+import {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} from 'sequelize-test-helpers';
+
+import assetModel from '../../src/database/models/asset.js';
+import orderModel from '../../src/database/models/order.js';
+
+const Asset = assetModel(sequelize, dataTypes);
+const Order = orderModel(sequelize, dataTypes);
+
+describe('Verifica se Asset model existe, se tem as suas propriedades e se possui as associações corretas', () => {
+
+  const asset = new Asset();
+
+  checkModelName(Asset)('Asset');
+
+  context('Verifica se existe todas as propriedades no model', () => {
+    [
+      'name',
+      'quantity',
+      'value',
+      'created',
+    ].forEach(checkPropertyExists(asset));
+  });
+
+  context('Verifica se existe todas as associações no model', () => {
+
+    before(() => {
+      Asset.associate({ Order });
+    });
+
+    it('Verifica se um Asset está em muitas Orders', () => {
+      expect(Asset.belongsToMany).to.have.been.calledWith(Order)
+    }) 
+    
+  });
+});
