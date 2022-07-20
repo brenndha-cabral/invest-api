d671eee4e63a9ebf4fd5425c44a123bbc7b28a10
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-20 20:14:10 -0300

    refactor: rename middlewares

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { validateFieldsLogin, validateRulesLogin, validateUserExist } = require('../middleware/validateLogin');
+const { validateFieldsLogin, validateRulesLogin } = require('../middleware/validateLogin');
+const { validateClientLogin } = require('../middleware/validateClient');
 
 const { loginController } = require('../controllers/loginController');
 
 const router = express.Router();
 
-router.post('/', validateFieldsLogin, validateRulesLogin, validateUserExist, loginController);
+router.post('/', validateFieldsLogin, validateRulesLogin, validateClientLogin, loginController);
 
 module.exports = router;
