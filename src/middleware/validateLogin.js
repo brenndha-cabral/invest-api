397de4c951db4d33b0c7cf10348acce4bc7e6b13
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-20 20:13:00 -0300

    refactor: translate messages

--- src/middleware/validateLogin.js
+++ src/middleware/validateLogin.js
@@ -1,15 +1,13 @@
 const { check, validationResult } = require('express-validator');
-const HttpException = require('../utils/httpException');
-const { statusCode, statusResponse } = require('../utils/httpStatus');
-const { Client } = require('../database/models/index');
+const { statusCode } = require('../utils/httpStatus');
 
 const validateFieldsLogin = [
   check('email')
     .isEmail()
-    .withMessage('Email inválido. Por favor, insira um email válido.'),
+    .withMessage('Invalid email. Please enter a valid email address.'),
   check('password')
     .isLength({ min: 6 })
-    .withMessage('A senha precisa ter no mínimo 6 dígitos. Por favor, insira uma senha válida.'),
+    .withMessage('The password must be at least 6 digits long. Please enter a valid password.'),
 ];
 const validateRulesLogin = (req, res, next) => {
   const errors = validationResult(req);
@@ -20,26 +18,7 @@ const validateRulesLogin = (req, res, next) => {
   return next();
 };
 
-const validateUserExist = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await Client.findOne({
-      where: { email, password },
-    });
-
-    if (!user) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Pessoa usuária não encontrada. Por favor, tente novamente.' });
-    }
-
-    return next();
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, statusResponse.INTERNAL_SERVER_ERROR);
-  }
-};
-
 module.exports = {
   validateFieldsLogin,
   validateRulesLogin,
-  validateUserExist,
 };
