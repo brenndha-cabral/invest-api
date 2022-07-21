9324b46028603f84a960de8a66195750f05b3b80
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-21 03:52:41 -0300

    feat: create getBalanceById controller

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,6 +1,6 @@
-const { getClientWithAssetService } = require('../services/clientService');
-const { HttpException } = require('../utils/httpException');
-const { statusCode, statusResponse } = require('../utils/httpStatus');
+const { getClientWithAssetService, balanceById } = require('../services/clientService');
+// const { HttpException } = require('../utils/httpException');
+const { statusCode } = require('../utils/httpStatus');
 
 const getClientWithAssetsByIdController = async (req, res) => {
   const { id } = req.params;
@@ -10,6 +10,15 @@ const getClientWithAssetsByIdController = async (req, res) => {
   return res.status(statusCode.OK).json(client);
 };
 
+const getBalanceById = async (req, res) => {
+  const { id } = req.params;
+
+  const response = await balanceById(id);
+
+  return res.status(statusCode.OK).json(response);
+};
+
 module.exports = {
   getClientWithAssetsByIdController,
+  getBalanceById,
 };
