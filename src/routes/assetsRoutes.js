c1f56a122c99039718b0f23a1db01bc07082cfbb
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-21 03:14:22 -0300

    feat: update asset route

--- src/routes/assetsRoutes.js
+++ src/routes/assetsRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { assetByIdController } = require('../controllers/assetControllers');
 const { getClientWithAssetsByIdController } = require('../controllers/clientController');
 
 const router = express.Router();
 
 router.get('/clientes/:id', getClientWithAssetsByIdController);
-router.get('/:id');
+router.get('/:id', assetByIdController);
 
 module.exports = router;
