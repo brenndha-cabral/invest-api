427b09f389d91d83f6cb4d8dc36e018a4d385f0b
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-20 22:08:58 -0300

    feat: create asset sell route

--- src/routes/assetRoutes.js
+++ src/routes/assetRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
-const { validateFieldsInvest, validateRulesInvest, validateAsset } = require('../middleware/validateAsset');
+const {
+  validateFieldsInvest,
+  validateRulesInvest,
+  validateBuyAsset,
+  validateSellAsset,
+} = require('../middleware/validateAsset');
 const { validateClientById } = require('../middleware/validateClient');
 
-const { buyAssetController } = require('../controllers/assetControllers');
+const { buyAssetController, sellAssetController } = require('../controllers/assetControllers');
 
 const router = express.Router();
 
-router.post('/comprar', validateFieldsInvest, validateRulesInvest, validateClientById, validateAsset, buyAssetController);
+router.post('/comprar', validateFieldsInvest, validateRulesInvest, validateClientById, validateBuyAsset, buyAssetController);
+router.post('/vender', validateFieldsInvest, validateRulesInvest, validateSellAsset, sellAssetController);
 
 module.exports = router;
