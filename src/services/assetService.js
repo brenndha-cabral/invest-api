8b5499cee13254ce356b927704e544b2dc14a911
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-20 22:07:21 -0300

    feat: create sellAssetService service

--- src/services/assetService.js
+++ src/services/assetService.js
@@ -35,6 +35,35 @@ const buyAssetService = async (codCliente, codAtivo, qtdeAtivo) => {
   return response;
 };
 
+const sellAssetService = async (codCliente, codAtivo, qtdeAtivo) => {
+  const { dataValues } = await Asset.findOne({
+    where: { id: codAtivo },
+  });
+
+  const newQuantity = dataValues.quantity + qtdeAtivo;
+
+  const response = await sequelize.transaction(async (t) => {
+    const order = await Order.create({
+      clientId: codCliente,
+      assetId: codAtivo,
+      quantity: qtdeAtivo,
+      type: 'sell',
+      value: dataValues.value,
+    }, { transaction: t });
+
+    await Asset.update(
+      { quantity: newQuantity },
+      { where: { id: codAtivo } },
+      { transaction: t },
+    );
+
+    return order;
+  });
+
+  return response;
+};
+
 module.exports = {
   buyAssetService,
+  sellAssetService,
 };
