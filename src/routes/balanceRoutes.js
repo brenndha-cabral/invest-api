bd3307f823bfe8ab70423fdba898859a35a25d99
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-21 03:49:18 -0300

    feat: create new balance routes

--- src/routes/balanceRoutes.js
+++ src/routes/balanceRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { balanceUpdateController } = require('../controllers/balanceController');
-const { validateFieldsBalance, validateRulesBalance, validateBalanceByClient } = require('../middleware/validateBalance');
+const { getBalanceById } = require('../controllers/clientController');
+const {
+  validateFieldsBalance,
+  validateRulesBalance,
+  validateBalanceDeposit,
+  validateBalanceWithdraw,
+} = require('../middleware/validateBalance');
 
 const router = express.Router();
 
-router.post('/deposito', validateFieldsBalance, validateRulesBalance, validateBalanceByClient, balanceUpdateController);
+router.post('/deposito', validateFieldsBalance, validateRulesBalance, validateBalanceDeposit, balanceUpdateController);
+router.post('/saque', validateFieldsBalance, validateRulesBalance, validateBalanceWithdraw, balanceUpdateController);
+router.get('/:id', getBalanceById);
 
 module.exports = router;
