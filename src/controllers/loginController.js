cfd9e0cc69515df0c118e47a72609d7b41cf93c6
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-22 19:38:02 -0300

    refactor: login controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,22 +1,17 @@
 const { loginService } = require('../services/loginService');
 const { HttpException } = require('../utils/httpException');
-const { statusCode, statusResponse } = require('../utils/httpStatus');
+const { statusCode } = require('../utils/httpStatus');
 
 const loginController = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const token = await loginService(email, password);
+  const token = await loginService(email, password);
 
-    if (!token) {
-      return res.status(statusCode.OK).json(token);
-    }
-
-    return res.status(statusCode.INTERNAL_SERVER_ERROR)
-      .json({ message: 'Unable to login. Please, try again.' });
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, statusResponse.INTERNAL_SERVER_ERROR);
+  if (!token) {
+    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, 'Unable to login. Please, try again.');
   }
+
+  return res.status(statusCode.OK).json(token);
 };
 
 module.exports = {
