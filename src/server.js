ecd30acbaa20cfc1937eb99e20bd380b532845a0
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-22 19:34:23 -0300

    feat: add errorMiddleware in server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require('express-async-errors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
 const routes = require('./routes');
-// const { errorMiddleware } = require('./middleware/errorMiddleware');
+const { errorMiddleware } = require('./middleware/errorMiddleware');
 
 const { PORT } = process.env;
 
@@ -16,6 +16,6 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/', routes);
 
-// app.use(errorMiddleware);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log('Listening at', PORT));
