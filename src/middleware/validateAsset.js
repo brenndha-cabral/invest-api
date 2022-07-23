d41400a526a40b02606256bff905dce9d73d713f
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-23 02:38:24 -0300

    refactor: update general validateAsset middleware

--- src/middleware/validateAsset.js
+++ src/middleware/validateAsset.js
@@ -1,84 +1,83 @@
 const { check, validationResult } = require('express-validator');
 const { HttpException } = require('../utils/httpException');
-const { statusCode, statusResponse } = require('../utils/httpStatus');
-const { Asset } = require('../database/models/index');
-const { calculateCustodyById } = require('../utils/calculateCustody');
+const { statusCode } = require('../utils/httpStatus');
+const { Asset, Client } = require('../database/models/index');
+const { calculateCustodyByIdHelper } = require('../utils/calculateCustody');
 
 const validateFieldsInvest = [
   check('codCliente')
     .exists()
     .withMessage('"codCliente" is required.')
     .isNumeric()
-    .withMessage('"codCliente" be must a number.'),
+    .withMessage('"codCliente" must be a number.'),
   check('codAtivo')
     .exists()
     .withMessage('"codAtivo" is required.')
     .isNumeric()
-    .withMessage('"codAtivo" be must a number.'),
+    .withMessage('"codAtivo" must be a number.'),
   check('qtdeAtivo')
     .exists()
     .withMessage('"qtdeAtivo" is required.')
     .isNumeric()
-    .withMessage('"qtdeAtivo" be must a number.'),
+    .withMessage('"qtdeAtivo" must be a number.'),
 ];
-const validateRulesInvest = (req, res, next) => {
+const validateRulesInvest = (req, _res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    const errorMessage = errors.array()[0].msg;
+    const errorMessage = (errors.array()[0]) ? errors.array()[0].msg : '';
 
     if (errorMessage.includes('required')) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: errorMessage });
+      throw new HttpException(statusCode.BAD_REQUEST, errorMessage);
     }
-    return res.status(statusCode.UNPROCESSABLE_ENTITY).json({ message: errorMessage });
+    throw new HttpException(statusCode.BAD_REQUEST, errorMessage);
   }
   return next();
 };
 
-const validateBuyAsset = async (req, res, next) => {
-  try {
-    const { codAtivo, qtdeAtivo } = req.body;
+const validateBuyAsset = async (req, _res, next) => {
+  const { codCliente, codAtivo, qtdeAtivo } = req.body;
 
-    const asset = await Asset.findOne({
-      where: { id: codAtivo },
-    });
+  const asset = await Asset.findOne({
+    where: { id: codAtivo },
+  });
 
-    if (!asset) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Asset not found. Please, try again.' });
-    }
+  const client = await Client.findOne({
+    where: { id: codCliente },
+  });
 
-    if (Number(qtdeAtivo) > asset.dataValues.quantity) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Asset quantity must be less or equal the available. Please, try again.' });
-    }
-
-    return next();
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, statusResponse.INTERNAL_SERVER_ERROR);
+  if (!asset) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Asset not found. Please, try again.');
   }
+
+  if (Number(qtdeAtivo) > asset.quantity) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Asset quantity must be less or equal the available. Please, try again.');
+  }
+
+  if ((asset.value * Number(qtdeAtivo)) > client.balance) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Client does not have sufficient balance for the transaction. Please, try again.');
+  }
+  return next();
 };
 
-const validateSellAsset = async (req, res, next) => {
-  try {
-    const { codCliente, codAtivo, qtdeAtivo } = req.body;
+const validateSellAsset = async (req, _res, next) => {
+  const { codCliente, codAtivo, qtdeAtivo } = req.body;
 
-    const asset = await Asset.findOne({
-      where: { id: codAtivo },
-    });
+  const asset = await Asset.findOne({
+    where: { id: codAtivo },
+  });
 
-    if (!asset) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Asset not found. Please, try again.' });
-    }
-
-    const assetCustody = await calculateCustodyById(codCliente, codAtivo);
-
-    if (Number(qtdeAtivo) > assetCustody) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Asset quantity must be less or equal the available or client does not have this asset. Please, try again.' });
-    }
-
-    return next();
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, statusResponse.INTERNAL_SERVER_ERROR);
+  if (!asset) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Asset not found. Please, try again.');
   }
+
+  const assetCustody = await calculateCustodyByIdHelper(codCliente, codAtivo);
+
+  if (Number(qtdeAtivo) > assetCustody) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Asset quantity must be less or equal the available or client does not have this asset. Please, try again.');
+  }
+
+  return next();
 };
 
 module.exports = {
