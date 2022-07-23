4125f0e58c44ec1b43bde837c1fbcc40e1173a4c
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-23 02:40:58 -0300

    refactor: update general validateToken middleware

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -3,23 +3,19 @@ const { statusCode } = require('../utils/httpStatus');
 const { authToken } = require('../utils/jwt');
 
 const validateToken = (req, _res, next) => {
-  try {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if (!token) {
-      throw new HttpException(statusCode.UNAUTHORIZED, 'Token not found');
-    }
-
-    const client = authToken(token);
-
-    if (client) {
-      return next();
-    }
-
-    throw new HttpException(statusCode.UNAUTHORIZED, 'Token invalid');
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, error.message);
+  if (!token) {
+    throw new HttpException(statusCode.UNAUTHORIZED, 'Token not found');
   }
+
+  const client = authToken(token);
+
+  if (client) {
+    return next();
+  }
+
+  throw new HttpException(statusCode.UNAUTHORIZED, 'Token invalid');
 };
 
 module.exports = {
