8518f6647627c5278e8605871d0a5c4c72cd1207
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-23 02:38:58 -0300

    feat: create validations middleware balance routes

--- src/middleware/validateBalance.js
+++ src/middleware/validateBalance.js
@@ -1,6 +1,6 @@
 const { check, validationResult } = require('express-validator');
 const { HttpException } = require('../utils/httpException');
-const { statusCode, statusResponse } = require('../utils/httpStatus');
+const { statusCode } = require('../utils/httpStatus');
 const { Client } = require('../database/models/index');
 
 const validateFieldsBalance = [
@@ -8,69 +8,79 @@ const validateFieldsBalance = [
     .exists()
     .withMessage('"codCliente" is required.')
     .isNumeric()
-    .withMessage('"codCliente" be must a number.'),
+    .withMessage('"codCliente" must be a number.'),
   check('valor')
     .exists()
     .withMessage('"valor" is required.')
     .isNumeric()
-    .withMessage('"valor" be must a number.'),
+    .withMessage('"valor" must be a number.'),
 ];
 const validateRulesBalance = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    const errorMessage = errors.array()[0].msg;
+    const errorMessage = (errors.array()[0]) ? errors.array()[0].msg : '';
 
     if (errorMessage.includes('required')) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: errorMessage });
+      throw new HttpException(statusCode.BAD_REQUEST, errorMessage);
     }
-    return res.status(statusCode.UNPROCESSABLE_ENTITY).json({ message: errorMessage });
+    throw new HttpException(statusCode.BAD_REQUEST, errorMessage);
   }
   return next();
 };
 
-const validateBalanceDeposit = async (req, res, next) => {
-  try {
-    const { codCliente, valor } = req.body;
+const validateBalanceDeposit = async (req, _res, next) => {
+  const { codCliente, valor } = req.body;
 
-    const client = await Client.findOne({
-      where: { id: codCliente },
-    });
+  const client = await Client.findOne({
+    where: { id: codCliente },
+  });
 
-    if (!client) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Client not found. Please, try again.' });
-    }
-
-    if (valor <= 0) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Value must be greater then zero. Please, try again.' });
-    }
-
-    return next();
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, statusResponse.INTERNAL_SERVER_ERROR);
+  if (!client) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Client not found. Please, try again.');
   }
+
+  if (valor <= 0) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Value must be greater then zero. Please, try again.');
+  }
+
+  return next();
 };
 
-const validateBalanceWithdraw = async (req, res, next) => {
-  try {
-    const { codCliente, valor } = req.body;
+const validateBalanceWithdraw = async (req, _res, next) => {
+  const { codCliente, valor } = req.body;
 
-    const client = await Client.findOne({
-      where: { id: codCliente },
-    });
+  const client = await Client.findOne({
+    where: { id: codCliente },
+  });
 
-    if (!client) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Client not found. Please, try again.' });
-    }
-
-    if (valor > client.dataValues.balance || valor <= 0) {
-      return res.status(statusCode.BAD_REQUEST).json({ message: 'Value must be greater then zero and available in balance. Please, try again.' });
-    }
-
-    return next();
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, statusResponse.INTERNAL_SERVER_ERROR);
+  if (!client) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Client not found. Please, try again.');
   }
+
+  if (Number(valor) > client.dataValues.balance || valor <= 0) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Value must be greater then zero and available in balance. Please, try again.');
+  }
+
+  return next();
+};
+
+const validateHasBalance = async (req, _res, next) => {
+  const { codCliente, valor } = req.body;
+
+  const client = await Client.findOne({
+    where: { id: codCliente },
+  });
+
+  if (!client) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Client not found. Please, try again.');
+  }
+
+  if (Number(valor) > client.dataValues.balance || valor <= 0) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Value must be greater then zero and available in balance. Please, try again.');
+  }
+
+  return next();
 };
 
 module.exports = {
@@ -78,4 +88,5 @@ module.exports = {
   validateRulesBalance,
   validateBalanceDeposit,
   validateBalanceWithdraw,
+  validateHasBalance,
 };
