ea3b8776e742a6fe494a0fde2f435346a604467a
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-23 20:36:52 -0300

    refactor: rename clients routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const { validateToken } = require('../middleware/validateToken');
 const loginRoutes = require('./loginRoute');
-const clientRoutes = require('./clientRoutes');
-const assetRoutes = require('./assetsRoutes');
+const clientsRoutes = require('./clientsRoutes');
+const assetsRoutes = require('./assetsRoutes');
 const investRoutes = require('./investRoutes');
 const balanceRoutes = require('./balanceRoutes');
 
 const router = express.Router();
 
 router.use('/login', loginRoutes);
-router.use('/clientes', clientRoutes);
-router.use('/ativos', validateToken, assetRoutes);
+router.use('/clientes', clientsRoutes);
+router.use('/ativos', validateToken, assetsRoutes);
 router.use('/investimentos', validateToken, investRoutes);
 router.use('/conta', validateToken, balanceRoutes);
 
