794d94163a33720c95f1fd60c44d4c6eae9d00f1
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-23 02:39:43 -0300

    feat: create validations middlewares clients

--- src/middleware/validateClient.js
+++ src/middleware/validateClient.js
@@ -1,26 +1,116 @@
+const { check, validationResult } = require('express-validator');
 const { HttpException } = require('../utils/httpException');
-const { statusCode, statusResponse } = require('../utils/httpStatus');
+const { statusCode } = require('../utils/httpStatus');
 const { Client } = require('../database/models/index');
+const { authToken } = require('../utils/jwt');
 
-const validateClientById = async (req, res, next) => {
-  try {
-    const { codCliente } = req.body;
+const validateFieldsNewClient = [
+  check('name')
+    .exists()
+    .withMessage('"name" is required')
+    .isString()
+    .withMessage('"name" must be a string')
+    .isLength({ min: 3 })
+    .withMessage('"name" length must be at least 3 characters long'),
+  check('email')
+    .exists()
+    .withMessage('"email" is required')
+    .isEmail()
+    .withMessage('"email" must be a email'),
+  check('password')
+    .exists()
+    .withMessage('"password" is required')
+    .isLength({ min: 6 })
+    .withMessage('"password" length must be at least 6 characters long'),
+  check('confirmPassword')
+    .exists()
+    .withMessage('"confirmPassword" is required')
+    .custom((val, { req }) => {
+      if (val !== req.body.password) {
+        throw new Error('Passwords are not the same. Please, try again.');
+      } else {
+        return val;
+      }
+    }),
+  check('cpf')
+    .exists()
+    .withMessage('"cpf" is required')
+    .isString()
+    .withMessage('"cpf" must be a string')
+    .isLength(11)
+    .withMessage('"cpf" length must be at 11 characters long'),
+  check('image')
+    .isString()
+    .withMessage('"image" must be a string'),
+  check('phone')
+    .exists()
+    .withMessage('"phone" is required')
+    .isString()
+    .withMessage('"phone" must be a string')
+    .isLength(12)
+    .withMessage('"phone" length must be at 12 characters long'),
+  check('address')
+    .exists()
+    .withMessage('"address" is required')
+    .isString()
+    .withMessage('"address" must be a string'),
+];
 
-    const client = await Client.findOne({
-      where: { id: codCliente },
-    });
+const validateRulesClient = (req, _res, next) => {
+  const errors = validationResult(req);
 
-    if (!client) {
-      throw new HttpException(statusCode.BAD_REQUEST, 'Client not found. Please, try again.');
+  if (!errors.isEmpty()) {
+    const errorMessage = (errors.array()[0]) ? errors.array()[0].msg : '';
+
+    if (errorMessage.includes('required')) {
+      throw new HttpException(statusCode.BAD_REQUEST, errorMessage);
     }
-
-    return next();
-  } catch (error) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, statusResponse.INTERNAL_SERVER_ERROR);
+    throw new HttpException(statusCode.BAD_REQUEST, errorMessage);
   }
+  return next();
 };
 
-const validateClientLogin = async (req, res, next) => {
+const validateClientById = async (req, _res, next) => {
+  const { codCliente } = req.body;
+
+  const client = await Client.findOne({
+    where: { id: codCliente },
+  });
+
+  if (!client) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Client not found. Please, try again.');
+  }
+
+  return next();
+};
+
+const validateClientIsAdm = async (req, _res, next) => {
+  const token = req.headers.authorization;
+
+  const client = authToken(token);
+
+  if (client.adm !== true) {
+    throw new HttpException(statusCode.UNAUTHORIZED, 'Client not authorized to add new asset. Only admins can add new assets.');
+  }
+
+  return next();
+};
+
+const validateClientByEmail = async (req, _res, next) => {
+  const { email } = req.body;
+
+  const client = await Client.findOne({
+    where: { email },
+  });
+
+  if (client) {
+    throw new HttpException(statusCode.BAD_REQUEST, 'Client already exist. Please, try again.');
+  }
+
+  return next();
+};
+
+const validateClientLogin = async (req, _res, next) => {
   const { email, password } = req.body;
 
   const client = await Client.findOne({
@@ -35,6 +125,10 @@ const validateClientLogin = async (req, res, next) => {
 };
 
 module.exports = {
+  validateFieldsNewClient,
+  validateRulesClient,
+  validateClientIsAdm,
+  validateClientByEmail,
   validateClientById,
   validateClientLogin,
 };
