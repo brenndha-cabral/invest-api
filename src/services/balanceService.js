f9e5c02564094706c657484f88d9885b774e3049
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-24 17:00:39 -0300

    feat: create balance history

--- src/services/balanceService.js
+++ src/services/balanceService.js
@@ -1,27 +1,44 @@
-const { Client } = require('../database/models');
+require('dotenv').config();
+const Sequelize = require('sequelize');
+const { Client, AccountOperation } = require('../database/models');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../database/config/config')[env];
+
+const sequelize = new Sequelize(config);
 
 const setBalanceUpdateService = async (codClient, value, path) => {
-  const client = await Client.findOne(
-    { where: { id: codClient } },
-  );
-
-  if (client) {
-    let updateBalance;
-
-    if (path === '/deposito') {
-      updateBalance = Number(client.balance) + Number(value);
-    } else if (path === '/saque') {
-      updateBalance = Number(client.balance) - Number(value);
-    }
-
-    const [update] = await Client.update(
-      { balance: updateBalance },
+  const newBalance = await sequelize.transaction(async (t) => {
+    const client = await Client.findOne(
       { where: { id: codClient } },
     );
-    return update; // Retorna 1 (uma linha alterada)
-  }
 
-  return null;
+    let updatedBalance;
+    let type;
+
+    if (path === '/deposito') {
+      updatedBalance = Number(client.balance) + Number(value);
+      type = 'deposit';
+    } else if (path === '/saque') {
+      updatedBalance = Number(client.balance) - Number(value);
+      type = 'withdraw';
+    }
+
+    const operation = await AccountOperation.create({
+      client_id: client.id,
+      amount: value,
+      type,
+    }, { transaction: t });
+
+    await Client.update(
+      { balance: updatedBalance },
+      { where: { id: codClient } },
+    );
+
+    return operation;
+  });
+
+  return newBalance;
 };
 
 module.exports = {
