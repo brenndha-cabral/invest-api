181bcbe855fb7f84698e32552883959125f9f7b6
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-24 04:15:02 -0300

    feat: update loginController

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,15 +1,10 @@
 const { loginService } = require('../services/loginService');
-const { HttpException } = require('../utils/httpException');
 const { statusCode } = require('../utils/httpStatus');
 
 const loginController = async (req, res) => {
   const { email, password } = req.body;
 
   const token = await loginService(email, password);
-
-  if (!token) {
-    throw new HttpException(statusCode.INTERNAL_SERVER_ERROR, 'Unable to login. Please, try again.');
-  }
 
   return res.status(statusCode.OK).json(token);
 };
